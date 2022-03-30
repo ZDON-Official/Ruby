d11cff949a44b9b5b655f2e1124082f74d4e2097
Zohãib R <80493433+ZDON-Official@users.noreply.github.com>
2022-03-30 00:45:06 -0400

    Add files via upload

---
+++ hello.rb
@@ -0,0 +1,27 @@
+puts "hello World!"
+
+# learn to use variables 
+
+hello_world = "Hello World!"
+puts hello_world
+
+fav_num = 20
+puts fav_num
+
+# concatenate
+puts hello_world + fav_num.to_s
+
+puts "#{hello_world} my favorite number is #{fav_num}"
+
+# get input from user
+
+#name = gets
+#puts name.capitalize
+
+puts Integer.ancestors
+
+puts "\n"
+
+#puts Integer.methods
+
+puts Math.sqrt(16)
